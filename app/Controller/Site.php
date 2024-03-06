4c3c9e696c517f800e78305db2d68efcfbaf45c8
FouHamster <alina63111@gmail.com>
2024-03-06 19:50:35 +0700

    registracia2

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -11,7 +11,7 @@ class Site
 {
     public function index(Request $request): string
     {
-        $posts = Post::where('id', $request->id)->get();
+        $posts = Post::all();
         return (new View())->render('site.post', ['posts' => $posts]);
     }
 
@@ -23,10 +23,11 @@ class Site
 
     public function signup(Request $request): string
     {
-        if ($request->method==='POST' && User::create($request->all())){
-            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        if ($request->method === 'POST' && User::create($request->all())) {
+            app()->route->redirect('/go');
         }
         return new View('site.signup');
     }
+
 
 }
